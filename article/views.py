a2c8384d95f57b968ff87f7c6f9991057a235060
HE-DE <201900130012@mail.sdu.edu.cn>
2021-02-05 10:20:35 +0800

    django

--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ArticlePost
 import markdown
@@ -33,17 +33,44 @@ def article_detail(request, id):
 
     return render(request, 'article/detail.html', context)
 
+
 def article_create(request):
-    if request.method=='POST':
-        article_post_form=ArticlePostForm(data=request.POST)
+    if request.method == 'POST':
+        article_post_form = ArticlePostForm(data=request.POST)
         if article_post_form.is_valid():
-            new_article=article_post_form.save(commit=False)
-            new_article.author=User.objects.get(id=1)
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
             new_article.save()
             return redirect("article:article_list")
         else:
             return HttpResponse("表单内容有误，请重新填写")
     else:
-        article_post_form=ArticlePostForm()
-        context={'article_post_form':article_post_form}
-        return render(request,'article/create.html',context)
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
+
+
+def article_safe_delete(request, id):
+    if request.method == 'POST':
+        article = ArticlePost.objects.get(id=id)
+        article.delete()
+        return redirect("article:article_list")
+    else:
+        return HttpResponse("仅允许POST请求")
+
+
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("表单数据有误")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article': article, 'article_post_form': article_post_form}
+        return render(request, 'article/update.html', context)
